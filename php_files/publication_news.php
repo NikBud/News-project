692b14d7ae826a13dd4c60a50f73602fa7009249
NikBud <nikbud03@gmail.com>
2024-05-01 00:24:57 +0200

    Implemented logic to create comments.

--- php_files/publication_news.php
+++ php_files/publication_news.php
@@ -1,27 +1,27 @@
 <?php
+    $newsData = [];
+    $searchStr = strtolower($_GET["search"]);
+    $theme = $_GET["theme"];
 
-$newsData = [];
-$searchStr = strtolower($_GET["search"]);
-$theme = $_GET["theme"];
-
-if (($handle = fopen("../csv_files/news.csv", "r")) !== false) {
-    while (($data = fgetcsv($handle, 1000, ",")) !== false) {
-        if ((strcasecmp($data[3], $theme) == 0) || (strcasecmp($theme, "Toutes") == 0)){
-            if ((strcasecmp($searchStr, "") == 0) || (strpos(strtolower($data[0]), $searchStr) !== false) || (strpos(strtolower($data[1]), $searchStr) !== false)){
-                $newsData[] = [
-                    'image' => $data[2],
-                    'title' => $data[0], 
-                    'content' => $data[1],
-                    'date' => $data[5],
-                ];
+    if (($handle = fopen("../csv_files/news.csv", "r")) !== false) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+            if ((strcasecmp($data[4], $theme) == 0) || (strcasecmp($theme, "Toutes") == 0)){
+                if ((strcasecmp($searchStr, "") == 0) || (strpos(strtolower($data[1]), $searchStr) !== false) || (strpos(strtolower($data[2]), $searchStr) !== false)){
+                    $newsData[] = [
+                        'id' => $data[0],
+                        'image' => $data[3],
+                        'title' => $data[1], 
+                        'content' => $data[2],
+                        'date' => $data[6],
+                    ];
+                }
             }
         }
+        fclose($handle);
     }
-    fclose($handle);
-}
 
 
-header('Content-Type: application/json');
-echo json_encode($newsData);
+    header('Content-Type: application/json');
+    echo json_encode($newsData);
 
 ?>
