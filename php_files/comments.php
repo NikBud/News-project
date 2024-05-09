f4d7968cf4d21213ba54592dff555e16def3c58f
Ruslan <rzinchenko96@gmail.com>
2024-05-09 22:49:31 +0200

    css style for comments

--- php_files/comments.php
+++ php_files/comments.php
@@ -40,9 +40,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Comments</title>
-    <link rel="stylesheet" type="text/css" href="../styles/comments.css">
     <script src="../scripts/comments.js"></script>
+    <link rel="stylesheet" type="text/css" href="../styles/comments.css">
 </head>
+
 <body>
     <div id="content-container">
         <div id="comments-container">
